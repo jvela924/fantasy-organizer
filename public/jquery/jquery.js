783b17c0247bada51219f0d80c964b6c94c5b481
Jeremy Vela <jvela924@gmail.com>
2019-08-06 15:50:22 -0400

    removed console log from jquery file

--- public/jquery/jquery.js
+++ public/jquery/jquery.js
@@ -4,7 +4,6 @@ $(() => {
     const $footballContainer = $('.footballContainer')
     const $footballTeam = $('.footballTeam')
     const $baseballTeam = $('.baseballTeam')
-    console.log($baseballContainer);
     $baseballContainer.append($baseballTeam)
     $footballContainer.append($footballTeam)
   }
